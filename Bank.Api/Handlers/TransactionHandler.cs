bb370c58d52f1f8c0956acd6193b404fe73cf7a2
Daniel <danielcarmo29@gmail.com>
2024-11-08 16:32:13 -0300

    Transaction 'Update' 'Get' 'Delete' concluidos

--- Bank.Api/Handlers/TransactionHandler.cs
+++ Bank.Api/Handlers/TransactionHandler.cs
@@ -5,6 +5,7 @@ using Bank.Core.Handlers;
 using Bank.Core.Models;
 using Bank.Core.Requests.Transactions;
 using Bank.Core.Responses;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bank.Api.Handlers;
 
@@ -36,23 +37,76 @@ public class TransactionHandler(AppDbContext context) : ITransactionHandler
       }
     }
 
-    public Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
+    public async Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
     {
-        throw new NotImplementedException();
+      try{
+      var transaction = await context
+      .Transactions
+      .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+    
+    if(transaction is null)
+      return new Response<Transaction?>(null, 404, "Transação não encontrada");
+
+
+    transaction.CategoryId = request.CategoryId;
+    transaction.Amount = request.Amount;
+    transaction.Title = request.Title;
+    transaction.Type = request.Type;
+    transaction.PaidOrReceivedAt = request.PaidOrReceivedAt;
+      
+      context.Transactions.Update(transaction);
+    await context.SaveChangesAsync();
+
+      return  new Response<Transaction?>(transaction);
+
+      }
+      catch
+      {
+        return new Response<Transaction?>(null, 500, "Não foi possivel buscar a transação");
+    }
+    }
+    public async Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
+    {
+      try{
+      var transaction = await context
+      .Transactions
+      .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+    
+        if(transaction is null)
+      return new Response<Transaction?>(null, 404, "Transação não encontrada");
+
+
+    context.Transactions.Remove(transaction);
+    await context.SaveChangesAsync();
+
+      return new Response<Transaction?>(transaction);
+
+      }
+      catch
+      {
+        return new Response<Transaction?>(null, 500, "Não foi possivel buscar a transação");
+      }  
     }
 
-    public Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
+    public async Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
     {
-        throw new NotImplementedException();
-    }
+ try{
+      var transaction = await context
+      .Transactions
+      .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+          return transaction is null
+            ? new Response<Transaction?>(null, 404, "Transação não encontrada")
+            : new Response<Transaction?>(transaction);
+        }
+        catch
+      {
+        return new Response<Transaction?>(null, 500, "Não foi possivel buscar a transação");
+    }      }
 
     public Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionByPeriodRequest request)
     {
         throw new NotImplementedException();
     }
 
-    public Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
-    {
-        throw new NotImplementedException();
-    }
 }
