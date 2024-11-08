0a4d84a8230b74da22b083e19429ad37072acd6a
Daniel <danielcarmo29@gmail.com>
2024-11-08 10:24:26 -0300

    Realizando os request das transactions

---
+++ Bank.Core/Requests/Transactions/DeleteTransactionRequest.cs
@@ -0,0 +1,6 @@
+namespace Bank.Core.Requests.Transactions;
+
+public class DeleteTransactionRequest : Request
+{
+  public long Id { get; set; }
+}
---
+++ Bank.Core/Requests/Transactions/GetTransactionByIdRequest.cs
@@ -0,0 +1,6 @@
+namespace Bank.Core.Requests.Transactions;
+
+public class GetTransactionByIdRequest : Request
+{
+   public long Id { get; set; }
+}
