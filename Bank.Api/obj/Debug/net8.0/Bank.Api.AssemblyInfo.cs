b07782c766e6988ee89fe59b9848be8cd416861f
Daniel <danielcarmo29@gmail.com>
2024-11-11 15:27:56 -0300

    Atualizaçao concluida e adiçao da "AddApiEndpoints"

--- Bank.Api/obj/Debug/net8.0/Bank.Api.AssemblyInfo.cs
+++ Bank.Api/obj/Debug/net8.0/Bank.Api.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("bc7ffd9b-9b09-4300-90bc-c64a933b925c")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cffdc239c2711f80857acd68c6be3ccb847f1c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Bank.Core/obj/Debug/net7.0/Bank.Core.AssemblyInfo.cs
+++ Bank.Core/obj/Debug/net7.0/Bank.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cffdc239c2711f80857acd68c6be3ccb847f1c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
